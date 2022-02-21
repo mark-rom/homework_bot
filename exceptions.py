3bce38ef6ec8b865aa560d10403149321db205e8
Павел Сергеев <pavel.al.sergeev@gmail.com>
2022-02-21 13:25:24 +0300

    First review changes. Add docstrings and unified language except for test purposes

--- exceptions.py
+++ exceptions.py
@@ -1,4 +1,5 @@
-class CustomException(Exception):
+class APIErrException(Exception):
+    """Custom Exception class for handling Practicum.Homeworks API errors."""
     def __init__(self, message):
         self.message = message
         super().__init__(self.message)
--- homework.py
+++ homework.py
@@ -2,12 +2,13 @@ import os
 import sys
 import time
 import logging
+from http import HTTPStatus
 
 from dotenv import load_dotenv
 import requests as r
 import telegram
 
-from exceptions import CustomException
+from exceptions import APIErrException
 
 load_dotenv()
 
@@ -27,113 +28,145 @@ HOMEWORK_STATUSES = {
     'rejected': 'Работа проверена: у ревьюера есть замечания.'
 }
 
+# logging
 logger = logging.getLogger(__name__)
 formatter = '%(asctime)s, %(levelname)s, %(message)s'
 handler = logging.StreamHandler(sys.stdout)
 logger.setLevel(logging.DEBUG)
 handler.setFormatter(formatter)
 logger.addHandler(handler)
-# могу ли я создать логгер в отдельном файле и импортнуть его сюда?
 
 
 def send_message(bot, message):
-    """I'll write docstrings and unify language till the next review."""
+    """
+    Sends message from the Bot to your Telegram.
+    Resieves an instance of telegram.Bot class as bot argument
+    and string as message.
+    """
     try:
         bot.send_message(TELEGRAM_CHAT_ID, text=message)
-        logger.info(f'В телеграм отправлено сообщение {message}')
+        logger.info(f'Bot have sent a message: {message}')
     except telegram.error.TelegramError(message):
-        logger.error(f'Бот не смог отправить сообщение {message}')
+        logger.error(f'Due to an error bot have not sent a message: {message}')
 
 
 def get_api_answer(current_timestamp):
-    """I'll write docstrings and unify language till the next review."""
+    """
+    Makes a request to Practicum.Homeworks API
+    and returns decoded json response.
+    Resieves a time in int format as a parameter.
+    """
     timestamp = current_timestamp or int(time.time())
     params = {'from_date': timestamp}
+
     try:
         response = r.get(ENDPOINT, headers=HEADERS, params=params)
-        s_code = response.status_code
-        if s_code != 200:
-            raise r.exceptions.HTTPError
-    except r.exceptions.HTTPError:
-        msg = f'Не доступен эндпойнт {ENDPOINT}, код ошибки {s_code}'
-        raise CustomException(msg)
-    else:
-        response = response.json()
-        return response
+    except r.exceptions.RequestException:
+        msg = 'Cannot reach the Endpoint.'
+        raise APIErrException(msg)
+
+    if response.status_code != HTTPStatus.OK:
+        msg = (f'Endpoint {ENDPOINT} is not availiable, '
+               f'http status: {response.status_code}'
+               )
+        raise APIErrException(msg)
+
+    return response.json()
 
 
 def check_response(response):
-    """I'll write docstrings and unify language till the next review."""
-    try:
-        hw_list = response['homeworks']
-        if type(hw_list) is not list:
-            msg = f'API вернул список домашних работ типа "{type(hw_list)}"'
-            raise TypeError(msg)
-    except KeyError:
-        msg = 'В ответе API нет ключа "homeworks"'
-        raise KeyError(msg)
-    else:
-        return hw_list
+    """
+    Checks if given response contains the "homeworks" key.
+    Returns it's value if it is a list.
+    """
+    if 'homeworks' not in response:
+        msg = 'There is no "homeworks" key in APIs response'
+        raise TypeError(msg)
+
+    hw_list = response['homeworks']
+
+    if not isinstance(hw_list, list):
+        msg = ('Type of "homeworks" value in APIs response'
+               f'is "{type(hw_list)}" not "list"'
+               )
+        raise APIErrException(msg)
+
+    return hw_list
 
 
 def parse_status(homework):
-    """I'll write docstrings and unify language till the next review."""
-    homework_name = homework.get('homework_name')
-    # нужна обработка отсутствия ключа homework_name
+    """
+    Takes a homework dictionary and returns
+    a string with the name and status of current homework.
+    """
+    if 'homework_name' in homework:
+        homework_name = homework.get('homework_name')
+    else:
+        msg = 'API have returned a homework without a "homework_name" key'
+        raise KeyError(msg)
     homework_status = homework.get('status')
 
     try:
         verdict = HOMEWORK_STATUSES[homework_status]
     except KeyError:
-        raise KeyError('API вернул неизвестный статус домашней работы')
-    else:
-        return f'Изменился статус проверки работы "{homework_name}". {verdict}'
+        msg = ('API have returned'
+               f'an unknown status {homework_status} for "{homework_name}"'
+               )
+        raise APIErrException(msg)
+
+    return f'Изменился статус проверки работы "{homework_name}". {verdict}'
 
 
 def check_tokens():
-    """I'll write docstrings and unify language till the next review."""
+    """
+    Returns True if PRACTICUM_TOKEN, TELEGRAM_TOKEN
+    and TELEGRAM_CHAT_ID variables aren't empty.
+    Returns False if one of them is empty.
+    """
     variables = (PRACTICUM_TOKEN, TELEGRAM_TOKEN, TELEGRAM_CHAT_ID)
     for variable in variables:
+
         if not variable:
             logger.critical(
-                f'Не определена переменная {variable}. '
-                'Работа программы прекращена'
+                f'Variable {variable} is not defined. '
+                'The bot is deactivated'
             )
             return False
+
     return True
 
 
 def main():
-    """Основная логика работы бота."""
-    check_tokens()
+    """General bot's logic."""
+    if check_tokens():
+        bot = telegram.Bot(token=TELEGRAM_TOKEN)
+        current_timestamp = int(time.time())
+        prev_upd_time = ''
 
-    bot = telegram.Bot(token=TELEGRAM_TOKEN)
-    current_timestamp = int(time.time())
-    prev_upd_time = ''
+        while True:
+            try:
+                response = get_api_answer(current_timestamp)
+                hw_list = check_response(response)
 
-    while True:
-        try:
-            response = get_api_answer(current_timestamp)
-            hw_list = check_response(response)
+                for homework in hw_list:
+                    upd_time = homework.get('date_updated')
 
-            if hw_list:
-                homework = hw_list[0]
-                upd_time = homework.get('date_updated')
+                    if upd_time != prev_upd_time:
+                        prev_upd_time = upd_time
+                        message = parse_status(homework)
+                        send_message(bot, message)
+                current_timestamp = int(time.time())
 
-                if upd_time != prev_upd_time:
-                    prev_upd_time = upd_time
-                    message = parse_status(homework)
-                    send_message(bot, message)
+            except APIErrException as error:
+                message = f'Сбой в работе программы: {error}'
+                logger.error(message)
+                send_message(bot, message)
 
-        except Exception as error:
-            message = f'Сбой в работе программы: {error}'
-            logger.error(message)
-            send_message(bot, message)
-        else:
-            current_timestamp = int(time.time())
-        finally:
-            time.sleep(RETRY_TIME)
-            check_tokens()
+            except Exception as error:
+                logger.error(error)
+
+            finally:
+                time.sleep(RETRY_TIME)
 
 
 if __name__ == '__main__':
